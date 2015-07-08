2f45408628dd521a67b3b54976d4080f4e2611b5
Kaare Hoff Skovgaard <khs@secoya.dk>
2015-07-08 16:51:02 +0200

    Added some deprecation comments

--- utils/index.js
+++ utils/index.js
@@ -8,6 +8,13 @@ import classNames from './class-names.js';
 export default {
 	isEmptyValue,
 	classNames,
+	/**
+	 * @deprecated Use classNames.merge instead
+	 */
 	mergeClassNames: classNames.merge,
+
+	/**
+	 * @deprecated Use classNames.conditionalAppend instead
+	 */
 	maybeAppendErrorClassNames: classNames.conditionalAppend,
 };
