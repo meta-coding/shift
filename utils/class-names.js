c67874a6b39e49bad4e619cd7fada922ad84d02e
Bjarke Bondo Andersen <bba@secoya.dk>
2015-07-13 21:33:21 +0200

    Change mergeNames slightly

--- utils/class-names.js
+++ utils/class-names.js
@@ -1,23 +1,11 @@
 /* @flow */
 export function mergeNames(...names: string[]) : string {
 	var classNames = {};
-	var classes, i, n;
-	var j, m;
-
-	// Fast path out
-	if (names.length === 1) {
-		return names[0];
-	}
-
-	for (i = 0, n = names.length; i < n; i++) {
-		classes = names[i].split(' ');
-		for (j = 0, m = classes.length; j < m; j++) {
-			if (classes[j].length === 0) {
-				continue;
-			}
-			classNames[classes[j]] = true;
-		}
-	}
+	names.forEach(name => {
+		name.split(' ')
+		.filter(className => className.length > 0)
+		.forEach(className => classNames[className] = true);
+	});
 	return Object.keys(classNames).join(' ');
 }
 
